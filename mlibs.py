dbc05a9b683798fe07dcd8d7b19ae910c71a3c88
jman13378 <garconiamail@gmail.com>
2022-10-09 14:42:41 -0400

    final before release

--- mlibs.py
+++ mlibs.py
@@ -14,8 +14,12 @@ import sys, os
 
 from pathlib import Path
 
-
-
+import os
+parent_dir = os.getenv('APPDATA')
+directory = "Money-Manager"
+file = "data.json"
+path = os.path.join(parent_dir,directory)
+print(path)
 def resource_path(relative_path):
     """ Get the absolute path to the resource, works for dev and for PyInstaller """
     try:
@@ -27,37 +31,47 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+dir_exists = os.path.exists(path)
+if not dir_exists:
+    os.mkdir(path)
 
-my_file = Path(resource_path("data.json"))
-if not my_file.is_file():
-    with open('data.json', "w") as a:
+path = os.path.join(path,file)
+print(os.path.exists(path))
+
+if not os.path.exists(path):
+    with open(path, 'x') as a:
         a.write(
             '{\n'
             +'  "bal": 0,\n'
             +'  "upgradeprice": 100,\n'
             +'  "upgrades": 0,\n'
             +'  "balper": 1,\n'
-            +'  "nextupprice": 50'
+            +'  "nextupprice": 50,\n'
+            +'  "prestige": 0'
             +'\n}')
         a.close()
 
+
 # Reading from file
-f = open (resource_path('data.json'), "r")
+f = open(path, 'r')
 data = json.loads(f.read())
+
+
+
+win=Tk()
+win.geometry("350x350")
+win.grid()
+win.config(bg="#2C2F33")
+win.title("Money Manager")
+
+imageIcon = Image.open(resource_path('favicon.ico'))
 
 bal = data["bal"]
 upgradeprice=data["upgradeprice"]
 upgrades=data["upgrades"]
 balper= data["balper"]
 nextupprice=data["nextupprice"]
-
-win=Tk()
-win.geometry("350x350")
-win.grid()
-win.config(bg="#0D1117")
-win.title("Money Manager")
-
-imageIcon = Image.open(resource_path('favicon.ico'))
+prestige=data['prestige']
 
 # Window icon parsing
 photo = ImageTk.PhotoImage(imageIcon)
@@ -74,12 +88,13 @@ def addbal():
 def ending():
     global bal, upgradeprice, balper, upgrades, nextupprice
     ans = askyesnocancel("Ending!", "You have Reached the end of the game"
-    + "\nClick YES to continue"
+    + "\nClick YES to Prestige"
     + "\nClick NO to exit(will save your data)"
     + "\nClick CANCEL to restart"
     )
     if ans:
-        showinfo("Continueing!","Continueing!")
+        showinfo("Prestiging!","Prestiging to: {}!".format(prestige+1))
+        prestigeup()
         pass
     elif not ans:
         showinfo("Exiting!","Exiting!")
@@ -92,34 +107,27 @@ def ending():
 
 
 
-def set_def_settings():
-    bal=0
-    upgradeprice=100
-    balper=1
-    upgrades=0
-    nextupprice=50
 def upgrade():
-    global bal, upgradeprice, balper, upgrades, nextupprice
+    global bal, upgradeprice, balper, upgrades, nextupprice,prestige
     if bal >= upgradeprice:
         upgrades += 1
         bal -= upgradeprice
-        balper += 1
+        balper += 1 + prestige
         cost = upgradeprice
         upgradeprice += nextupprice
         if upgrades == 3:
-            nextupprice = 100
+            nextupprice = 100*prestige
         elif upgrades == 6:
-            nextupprice = 150
+            nextupprice = 150*prestige
         elif upgrades == 9:
-            nextupprice = 200
+            nextupprice = 200*prestige
         elif upgrades == 13:
-            nextupprice = 300
+            nextupprice = 300*prestige
         elif upgrades == 15:
-            nextupprice = 450
+            nextupprice = 450*prestige
         elif upgrades == 18:
-            nextupprice = 600
-        elif upgrades == 20:
-            nextupprice = 1000
+            nextupprice = 600*prestige
+        elif (upgrades == (20 + (10 * prestige))):
             ending()
 
         reload_bal()
@@ -127,18 +135,28 @@ def upgrade():
         reload_upgrade_level()
         playsound(resource_path('complete.mp3'))
         showinfo("Upgraded!", "You have upgraded your MPS(Money per second)\nUpgrade: {}\nCost: {}\nRemaining Bal: {}\nNext Cost: {}".format(upgrades, cost, bal, upgradeprice) )
-        
+
     else:
         playsound(resource_path('error.mp3'))
         showinfo('Not Enought Money!', "Sorry, you doen't have enough money for this!\nCurrent Bal: {}\nNeeded: {}".format(bal, upgradeprice))
         
+def prestigeup():
+    prestige += 1
+    bal=0
+    upgradeprice=150
+    upgrades=0
+    balper= 1+prestige
+    nextupprice=50
 
 def reload_bal():
     global bal
     balance = Label( win, text="Money: ${}".format(bal), bg="#0D1117", fg="white").grid(column=1,row=0)
+
 def reload_upgrades():
     global upgradeprice
     upgr = Label( win, text="Upgrade Price: ${}".format(upgradeprice), bg="#0D1117", fg="white").grid(column=1,row=1)
+
+
 def reload_upgrade_level():
     global upgrades
     upgrlstart = Label( win, text="Level: {}".format(upgrades), bg="#0D1117", fg="white").grid(column=2,row=1)
@@ -147,17 +165,19 @@ def reload_upgrade_level():
 #scheduler = BackgroundScheduler()
 #scheduler.add_job(reload_bal, 'interval', minutes=30)
 #scheduler.start()
+
+
 def close():
-    global bal, upgradeprice, balper, upgrades, nextupprice
-    a = open (resource_path('data.json'), "w")
-    a.truncate
+    global bal, upgradeprice, balper, upgrades, nextupprice,prestige
+    a = open(path, "w")
     a.write(
         '{\n'
         +'  "bal": {},\n'.format(bal)
         +'  "upgradeprice": {},\n'.format(upgradeprice)
         +'  "upgrades": {},\n'.format(upgrades)
         +'  "balper": {},\n'.format(balper)
-        +'  "nextupprice": {}'.format(nextupprice)
+        +'  "nextupprice": {},\n'.format(nextupprice)
+        +'  "prestige": {}'.format(prestige)
         +'\n}')
     a.close()
     win.destroy()
@@ -165,16 +185,20 @@ def close():
 def restart():
     ans = askyesno("Reset Warning!", "WOULD YOU LIKE TO RESET ALL OF YOUR DATA!")
     if ans:
-        set_def_settings()
+        global bal, upgradeprice, balper, upgrades, nextupprice, prestige
+        bal=0
+        prestige= 0
+        upgradeprice=100
+        balper=1
+        upgrades=0
+        nextupprice=50
         close()
     else:
         pass
-
-upgrstart = Label( win, text="Upgrade Price: ${}".format(upgradeprice), bg="#0D1117", fg="white").grid(column=1,row=1)
-upgrlstart = Label( win, text="Upgrade Level: ${}".format(upgrades), bg="#0D1117", fg="white").grid(column=2,row=1)
+reload_bal()
+reload_upgrades()
+reload_upgrade_level()
 upgr = Button(win,text="Upgrade", command=upgrade, bg="#0D1117", fg="white").grid(column=3, row=1)
-
-balstart = Label( win, text="Money: ${}".format(bal), bg="#0D1117", fg="white").grid(column=1,row=0)
 
 addmoney = Button(win,text="Add Money", command=addbal, bg="#0D1117", fg="white").grid(column=2, row=0)
 
